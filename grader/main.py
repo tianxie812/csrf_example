843ce7bf0c7df0aea44d6383cb2da9ce56f98712
tianxie812 <xietian628@yahoo.com>
2020-09-19 11:27:29 -0700

    done

--- grader/main.py
+++ grader/main.py
@@ -1,5 +1,6 @@
 import os
 import base64
+import random
 
 from flask import Flask, request, session
 from model import Grade 
@@ -9,15 +10,18 @@ app.secret_key = "dFbVyccvTuNpkUP2x6vcnZWN"
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if 'csrf_token' not in session:
+        session['csrf_token'] = str(random.randint(10000000, 99999999))
 
     if request.method == 'POST':
-        g = Grade(
-            student=request.form['student'],
-            assignment=request.form['assignment'],
-            grade=request.form['grade'],
-        )
-        #print("(" + request.form['grade'] + ")")
-        g.save()
+        if request.form.get('csrf_token', None) == session['csrf_token']:
+            g = Grade(
+                student=request.form['student'],
+                assignment=request.form['assignment'],
+                grade=request.form['grade'],
+            )
+            #print("(" + request.form['grade'] + ")")
+            g.save()
 
     body = """
 <html>
